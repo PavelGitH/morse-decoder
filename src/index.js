f8fbdb76c110ad1ee75c0f52f86113d55dfd0d19
Pavel <76850802+PavelGitH@users.noreply.github.com>
2023-01-17 16:35:48 +0300

    feat: add solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let answer = '';
+
+  let arrayExpr = expr.split('');
+  let spellExpr = [];
+  let numLitters = expr.length / 10;
+
+  for (let i = 0; i < numLitters; i++) {
+    spellExpr.push(arrayExpr.splice(0, 10).join(''));
+  }
+  console.log(spellExpr);
+
+  MORSE_TABLE_BC = {};
+  for (let key in MORSE_TABLE) {
+    let keyArrayMT = key.split('');
+    let keyArrayMTB = [];
+    for (let i = 0; i < keyArrayMT.length; i++) {
+      if (keyArrayMT[i] == '.') {
+        keyArrayMTB.push('10');
+      }
+      if (keyArrayMT[i] == '-') {
+        keyArrayMTB.push('11');
+      }
+    }
+    keyArrayMTB.unshift('00', '00', '00', '00');
+    MORSE_TABLE_BC[keyArrayMTB.slice(-5).join('')] = MORSE_TABLE[key];
+  }
+  MORSE_TABLE_BC['**********'] = ' ';
+
+
+  for (let j = 0; j < spellExpr.length; j++) {
+    answer += MORSE_TABLE_BC[spellExpr[j]];
+  }
+
+  return answer;
 }
 
 module.exports = {
